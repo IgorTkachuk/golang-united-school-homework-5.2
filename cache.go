1178a28073540b94146e8ca9236c4ed3852e03ee
Igor Tkachuk <I.Tka4yk@gmail.com>
2022-06-03 23:05:57 +0300

    feat: add solution

--- cache.go
+++ cache.go
@@ -1,23 +1,56 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type cacheEntry struct{
+	value string
+	isInfinit bool
+	deadline time.Time
+}
 
 type Cache struct {
+	data map[string]cacheEntry
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{data: make(map[string]cacheEntry)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
+	if _, ok := c.data[key]; !ok { return "", false }
+	if !c.data[key].isInfinit && time.Now().After(c.data[key].deadline) {return "", false}
 
+	return c.data[key].value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.data[key] = struct {
+		value string
+		isInfinit bool
+		deadline time.Time
+	}{
+		value: value,
+		isInfinit: true,
+		deadline: time.Now(),
+	}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var res []string
+	
+	for k, _ := range c.data {
+		res = append(res, k)
+	}
+
+	return res
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
-}
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.Put(key, value)
+	t := c.data[key]
+	t.isInfinit = false
+	t.deadline = deadline
+	c.data[key] = t
+}	
